aabe31cd440deeca88e16ddf749abff26535f8e5
Martin <martinbampton@hotmail.com>
2018-11-17 21:41:25 +0000

    Starting seeders...

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $decks[] = ['name' => 'Standard'];
+        $decks[] = ['name' => 'Dark City'];
+        $decks[] = ['name' => 'Fantastic Four'];
+        $decks[] = ['name' => 'Guardians of the Galaxy'];
+        $decks[] = ['name' => 'Deadpool'];
+        $decks[] = ['name' => 'Paint The Town Red'];
+        $decks[] = ['name' => 'Civil War'];
+
+        \App\Deck::insert($decks);
+
+        $decks = \App\Deck::all();
+
+        $original = $decks->where('name', 'Standard')->first()->id;
+
+
+
     }
 }
