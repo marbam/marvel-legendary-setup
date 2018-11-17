84f3d7e3c93c0d540d3af46189f52a26ab0abc1f
Martin <martinbampton@hotmail.com>
2018-11-17 19:14:22 +0000

    Standard Laravel stuff

---
+++ config/custom_data.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'list' => [
+        'players' => [1,2,3,4,5],
+        'packs' => [
+            'standard' => 'Standard',
+            'dark' => 'Dark City',
+            'four' => 'Fantastic Four',
+            'gotg' => 'Guardians of the Galaxy',
+            'deadpool' => 'Deadpool',
+            'civil' => 'Civil War',
+            'paint' => 'Paint the Town Red',
+            'cap' => 'Captain America',
+            //'Villains'
+        ],
+    ]
+];
